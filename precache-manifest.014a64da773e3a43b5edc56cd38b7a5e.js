58c3f9894db68bbdb8fdabf8dade86fd9e4b31d6
Alexander Dolgov <sanhoxd@gmail.com>
2019-12-02 02:54:26 +0300

    Updates

---
+++ precache-manifest.014a64da773e3a43b5edc56cd38b7a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55bf42d0990a64eeb00148d0da03d21",
+    "url": "/stonksCharts/index.html"
+  },
+  {
+    "revision": "ff3efd127ee94abf5ab3",
+    "url": "/stonksCharts/static/css/main.13e1de07.chunk.css"
+  },
+  {
+    "revision": "82b75b6156f675cbf7bd",
+    "url": "/stonksCharts/static/js/2.aff721e6.chunk.js"
+  },
+  {
+    "revision": "ff3efd127ee94abf5ab3",
+    "url": "/stonksCharts/static/js/main.c6f0b30c.chunk.js"
+  },
+  {
+    "revision": "7e4398b054e903ae7b2a",
+    "url": "/stonksCharts/static/js/runtime-main.e4a38e05.js"
+  }
+]);
